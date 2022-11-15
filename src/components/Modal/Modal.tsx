282f0c4a3fbc372d9e0efa9ad6a3e2875f72cb74
danieldamian09 <humbertorivero9@gmail.com>
2022-11-15 16:48:36 -0300

    Feat style modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import {createUseStyles} from "react-jss";
+import "./style.css";
+
+const useStyle = createUseStyles({
+	modalOverlay: {
+		position: "fixed",
+		top: "0",
+		left: "0",
+		width: "100vw",
+		height: "100vh",
+		zIndex: "999",
+		backgroundColor: "rgba(14, 14, 14, 0.5)",
+		overflowY: "auto",
+	},
+	modalWrapper: {
+		position: "absolute",
+		top: "56px",
+		left: "0",
+		zIndex: "9999",
+		width: "100%",
+		height: "100%",
+		outline: "0",
+		display: "flex",
+		alignItems: "center",
+	},
+	modal: {
+		backgroundColor: "rgb(237, 237, 237)",
+		position: "relative",
+		margin: "auto",
+		borderRadius: "4px",
+		maxWidth: "700px",
+		padding: "24px",
+	},
+	modalHeader: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		"& h4": {
+			fontSize: "24px",
+		},
+	},
+	modalCloseButton: {
+		fontSize: "16px",
+		fontWeight: "600",
+		color: "rgb(20, 20, 20)",
+		cursor: " pointer",
+		padding: " 8px 16px",
+		border: "none",
+		backgroundColor: "rgb(0, 190, 171)",
+		borderRadius: "4px",
+	},
+});
 
 export interface ModalInterface {
 	isShowing: boolean;
@@ -9,27 +60,35 @@ export interface ModalInterface {
 	children: React.ReactNode;
 }
 
-const Modal : React.FC<ModalInterface> = ({ isShowing, hide, title, children}) => {
+const Modal: React.FC<ModalInterface> = ({
+	isShowing,
+	hide,
+	title,
+	children,
+}) => {
+
+  const classes = useStyle();
+
 	return isShowing
-    ? ReactDOM.createPortal(
-      <>
-        <div className="modal-overlay">
-          <div className="modal-wrapper">
-            <div className="modal">
-              <div className="modal-header">
-                <h4>{title}</h4>
-                <button className="modal-close-button" onClick={hide}>
-                  Cerrar
-                </button>
-              </div>
-              <div className="modal-body">{children}</div>
-            </div>
-          </div>
-        </div>
-      </>,
-      document.body
-    )
-    : null;
+		? ReactDOM.createPortal(
+				<>
+					<div className={classes.modalOverlay}>
+						<div className={classes.modalWrapper}>
+							<div className={classes.modal}>
+								<div className={classes.modalHeader}>
+									<h4>{title}</h4>
+									<button className={classes.modalCloseButton} onClick={hide}>
+										Cerrar
+									</button>
+								</div>
+								<div>{children}</div>
+							</div>
+						</div>
+					</div>
+				</>,
+				document.body
+		  )
+		: null;
 };
 
 export default Modal;
