d1c8b048a38963e554035d69d201a9e463e355e3
danieldamian09 <humbertorivero9@gmail.com>
2022-11-15 17:12:48 -0300

    Add example button

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Example } from './components/Example'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Example />
   </React.StrictMode>
 )
